e714ee0c84835628c0503bc1c9bca50ba6e8a806
franc <francesco.claudiani@targatelematics.com>
2022-12-06 17:25:53 +0100

    aggiunta funzione SliceNst() che restituisce il numero ordinato

--- conv.go
+++ conv.go
@@ -1,14 +1,26 @@
 package conv
 
+import (
+	"strconv"
+	"strings"
+)
+
 func SliceN(n int) []int {
-	cont, x := 0, n
-	for x != 0 {
-		x /= 10
+	var sliceN []int
+	cont := 0
+	for n != 0 {
+		sliceN = append(sliceN, n%10)
+		n /= 10
 		cont++
 	}
-	sliceN := make([]int, cont)
-	for i := 0; i < cont; i++ {
-		sliceN = append(sliceN, n%10)
-	}
 	return sliceN
+}
+
+func SliceNst(n int) []int {
+	cifre := strings.Split(strconv.Itoa(int(n)), "")
+	s := make([]int, len(cifre))
+	for i, r := range cifre {
+		s[i], _ = strconv.Atoi(r)
+	}
+	return s
 }
